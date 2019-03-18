232ea63050b3426ce0666f3dc40fbd379a58aae0
jmarkman <jmarkman446@gmail.com>
2019-03-18 16:21:26 -0400

    various bits moving forward simultaneously - added enum class for possible functionality - added viewmodel for weather app itself - built out WeatherClient class - added xaml-based datacontext for future

--- WeatherAPI/WeatherClient.cs
+++ WeatherAPI/WeatherClient.cs
@@ -1,16 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
 
 namespace WeatherAPI
 {
     public class WeatherClient
     {
-        public WeatherClient()
-        {
+        private string apiKey;
+        private string urlParams;
+        private static HttpClient httpClient = new HttpClient();
 
+        public int ZipCode { get; set; }
+
+        public WeatherClient(string apiKey)
+        {
+            this.apiKey = apiKey;
+            httpClient.BaseAddress = new Uri("https://api.openweathermap.org/data/2.5/weather");
+        }
+
+        public async Task<WeatherData> GetWeatherDataAsync(int zipCode)
+        {
+            ZipCode = zipCode;
+            WeatherData weatherData = null;
+            urlParams = $"weather?zip={ZipCode},us&appid={apiKey}";
+
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var response = await httpClient.GetAsync(urlParams);
+            if (response.IsSuccessStatusCode)
+            {
+                var data = await response.Content.ReadAsStringAsync();
+                weatherData = JsonConvert.DeserializeObject<WeatherData>(data);
+            }
+
+            return weatherData;
         }
     }
 }
---
+++ WeatherApp/ViewModel/WeatherViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using WeatherAPI;
+
+namespace WeatherApp.ViewModel
+{
+    public class WeatherViewModel
+    {
+        private WeatherClient client = new WeatherClient("aaa");
+
+        public WeatherData WeatherData { get; set; }
+
+        public WeatherViewModel()
+        {
+            
+        }
+
+        public void GetWeather(int zipCode)
+        {
+            WeatherData = client.GetWeatherDataAsync(zipCode).Result;
+        }
+        
+    }
+}
