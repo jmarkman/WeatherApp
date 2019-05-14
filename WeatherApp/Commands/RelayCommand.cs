a88d6a8fee265306a4160b135d6974e6da5c2f4a
jmarkman <jmarkman446@gmail.com>
2019-05-14 16:53:49 -0400

    got some initial feedback from the csharp discord

--- WeatherApp/Commands/RelayCommand.cs
+++ WeatherApp/Commands/RelayCommand.cs
@@ -5,24 +5,63 @@ namespace WeatherApp.Commands
 {
     public class RelayCommand : ICommand
     {
-        private Action _action;
+        //private Action _action;
 
-        public event EventHandler CanExecuteChanged = (sender, e) => { };
+        //public event EventHandler CanExecuteChanged = (sender, e) => { };
 
 
-        public RelayCommand(Action action)
+        //public RelayCommand(Action action)
+        //{
+        //    _action = action;
+        //}
+
+        //public bool CanExecute(object parameter)
+        //{
+        //    return true;
+        //}
+
+        //public void Execute(object parameter)
+        //{
+        //    _action();
+        //}
+
+        private readonly Predicate<object> _canExecute;
+        private readonly Action _execute;
+
+        public event EventHandler CanExecuteChanged;
+
+        public RelayCommand(Action execute)
+                       : this(execute, null)
         {
-            _action = action;
+        }
+
+        public RelayCommand(Action execute,
+                       Predicate<object> canExecute)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
         }
 
         public bool CanExecute(object parameter)
         {
-            return true;
+            if (_canExecute == null)
+            {
+                return true;
+            }
+
+            return _canExecute(parameter);
         }
 
         public void Execute(object parameter)
         {
-            _action();
+            _execute();
         }
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+
+
     }
 }
